bad9bbb075820c828fbe94721ce99d3195ed79f7
Alexander Manhart <alexander@manhart-it.de>
2023-01-13 19:52:50 +0100

    MailCatcher did not listen on port 2525 because of missing parameter. Time zone for the system and PHP is configured to Europe/Berlin by default.

--- scripts/mail.sh
+++ scripts/mail.sh
@@ -9,7 +9,7 @@ echo '==============================================================='
 echo '===>                    Install Postfix                    <==='
 echo '==============================================================='
 echo ''
-debconf-set-selections <<< "postfix postfix/mailname string default-lamp.local"
+debconf-set-selections <<< "postfix postfix/mailname string lamp.local"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get install -yqq --assume-yes postfix
 echo '* smtp:[localhost]:2525' > /etc/postfix/transport
@@ -26,14 +26,14 @@ echo ''
 #
 apt-get install -yqq rubygems ruby-dev sqlite3 libsqlite3-dev
 
-gem install net-imap -v 0.2.2
-gem install net-protocol -v 0.1.2
-gem install net-smtp -v 0.3.0
+gem install net-imap
+gem install net-protocol
+gem install net-smtp
 gem install mailcatcher
 
-mv /tmp/files/systemd/mailcatcher.service /etc/systemd/system/mailcatcher.service
-chown root:root /etc/systemd/system/mailcatcher.service
-chmod 744 /etc/systemd/system/mailcatcher.service
+mv /tmp/files/systemd/mailcatcher.service /lib/systemd/system/mailcatcher.service
+chown root:root /lib/systemd/system/mailcatcher.service
+chmod 744 /lib/systemd/system/mailcatcher.service
 systemctl enable mailcatcher.service
 
 exit 0
